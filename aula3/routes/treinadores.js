f6feb30fd5e5655303092642ad903e8709a4c069
Piimeentaa <dora13.isa@gmail.com>
2019-12-03 22:23:13 -0300

    Aula atualizado com jsonweb

--- aula3/routes/treinadores.js
+++ aula3/routes/treinadores.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../controllers/TreinadoresController")
 
+
 router.get('', controller.getAll)
 router.post('', controller.add)
 router.get('/:id', controller.getById)
@@ -10,8 +11,9 @@ router.patch('/:id', controller.update)
 router.delete('/:id', controller.remove)
 router.post('/:treinadorId/pokemons', controller.addPokemon)
 router.patch('/:treinadorId/pokemons/:pokemonId/treinar', controller.treinarPokemon)
-router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
+router.get('/:id/pokemons', controller.getPokemons)
 router.patch('/:treinadorId/pokemons/:pokemonId', controller.updatePokemon)
+router.get('/:treinadorId/pokemons/:pokemonId', controller.getPokemonById)
 router.post('/login', controller.login)
 
 module.exports = router
