d64bf3af198b066dbb317887cfe1f286cff7aba3
Piimeentaa <dora13.isa@gmail.com>
2019-12-06 21:20:46 -0300

    Colocando o procfile, o .env para a raiz

--- aula3/app.js
+++ aula3/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const pokemons = require('./routes/pokemons')
 const treinadores = require('./routes/treinadores')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json())
